3fe24fd3ed40225c1f484f3879a19d4b7c4665f1
cy3589 <jcy3589@naver.com>
2021-11-10 04:49:14 +0900

    backUrl {}로 감쌈

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -2,7 +2,7 @@ import { all, fork } from "redux-saga/effects";
 import postSaga from "./post";
 import userSaga from "./user";
 import axios from "axios";
-import backUrl from "../config/config";
+import { backUrl } from "../config/config";
 axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 export default function* rootSaga() {
