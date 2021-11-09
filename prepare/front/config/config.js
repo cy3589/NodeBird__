544f1e40d70e8d64f93d19706967fb83426e0571
cy3589 <jcy3589@naver.com>
2021-11-10 04:32:35 +0900

    back단 url 변수화, 빌드테스트 완료

---
+++ prepare/front/config/config.js
@@ -0,0 +1,1 @@
+export const backUrl = "http://52.79.104.197";
--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -2,7 +2,8 @@ import { all, fork } from "redux-saga/effects";
 import postSaga from "./post";
 import userSaga from "./user";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3065";
+import backUrl from "../config/config";
+axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 export default function* rootSaga() {
   yield all([fork(postSaga), fork(userSaga)]);
