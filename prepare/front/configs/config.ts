8db9cc4d1cec8bdb28ff8bb66459bba51847f761
정찬영 <jcy3589@gmail.com>
2022-03-04 12:34:58 +0000

    backUrl Update

--- prepare/front/configs/config.ts
+++ prepare/front/configs/config.ts
@@ -1,8 +1,6 @@
 export const backUrl =
   process.env.NODE_ENV === 'production'
-    ? // ? 'http://api.mynodesns.shop'
-      'http://192.168.219.117:3065'
-    : // : "http://localhost:3065";
-      'http://192.168.219.117:3065';
+    ? 'https://api-nodebird.cy3589.com'
+    : 'http://192.168.219.117:3065';
 // export const backUrl = "http://localhost:3065";
 export default backUrl;
